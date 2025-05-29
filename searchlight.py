1fdc7dd5d1f80ff11f338244eb1de0867e49501a
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2025-05-28 22:50:47 -0400

    Surface based searchlight

--- searchlight.py
+++ searchlight.py
@@ -12,7 +12,7 @@ import pandas as pd
 import nibabel as nb
 import nitools as nt
 import h5py # For IO with HDF5 files
-
+import AnatSearchlight.pymvpa_surf as nno  # Nick Oosterhof's library for dijkstra distrance
 
 def load(fname):
     """Loads a searchlight definition from an HDF5 file
@@ -243,8 +243,110 @@ class SearchlightVolume(Searchlight):
             self.maxdist[i] = np.max(dist[vi])
 
 class SearchlightSurface(Searchlight):
-    def __init__(self):
-        pass
+    """ Surface-based searchlights for a single hemisphere, given a individual surface.
+    Voxels we picked from the mask_img, for each vertex on the surface.
+    """
+
+    def __init__(self,structure ='none',depths=[0,0.2,0.4,0.6,0.8,1]):
+        """Constructor for SearchlightVolume with either fixed radius or fixed number of voxels.
+        If both a set to a value, nvoxels is used up to a maximum of the radius.
+
+        Args:
+            structure (str):
+                structure name for the cifti file (e.g. 'left_hem', 'right_hem').
+        """
+        self.classname ='SearchlightSurface'
+        self.structure = structure
+        self.depths = np.array(depths)  # Depth for sampling
+        self.n_points = len(self.depths)  # Number of points to sample
+
+    def define(self,surfs,mask_img,roi=None,radius=10,nvoxels=None):
+        """ Calculates the voxel_list for a Volume-based searchlight .
+
+        Args:
+            surfs (list of strs):
+                List of surface gifti file names used to define the searchlight centers.
+            mask_img (filename or NiftiImage):
+                Mask image to define input space (voxels to be used in the searchlight)
+            roi (filename, GiftiImage, ndrarray):
+                Define searchlight centers. If not given, the searchlight centers are calculated for all vertices.
+            radius (float):
+                Maximum searchlight radius - set to None if you only want a fixed number of voxels
+            nvoxels (int):
+                Number of voxels in the searchlight. If not given, the searchlight will be defined by a constant radius.
+        """
+        # Get the two surfaces
+        if not isinstance(surfs,list):
+            raise ValueError("surfs must be a list")
+        self.surfs = surfs
+        surfaces = [nb.load(f) for f in surfs]
+
+        # Check that the surfaces are compatible
+        if len(surfaces) != 2:
+            raise ValueError("surfs must contain exactly two surfaces")
+        if not all([isinstance(s,nb.gifti.GiftiImage) for s in surfaces]):
+            raise ValueError("Surfaces must be GiftiImage objects")
+        c1 = surfaces[0].darrays[0].data
+        c2 = surfaces[1].darrays[0].data
+        f1 = surfaces[0].darrays[1].data
+        if c2.shape[0] != c1.shape[0]:
+            raise ValueError("Surfaces must have the same number of vertices")
+        self.n_vertices = c1.shape[0]  # Number of vertices in the surface
+
+        # Get the mask image
+        if isinstance(mask_img,str):
+            self.mask_img = nb.load(mask_img)
+        elif isinstance(mask_img,nb.Nifti1Image):
+            self.mask_img = mask_img
+        else:
+            raise ValueError("roi_img must be a filename or Nifti1Image")
+
+        # Use the roi defition
+        if (roi is None):
+            self.center_indx = np.arange(self.n_vertices).astype('int16') # Index of each center
+        elif isinstance(roi,np.ndarray):
+            if roi.ndim == 1:
+                self.center_indx = roi.astype('int16')
+            else:
+                raise ValueError("roi must be a 1d array")
+        else:
+            raise ValueError("Gitfi image ROI not implemented yet")
+        self.n_cent = self.center_indx.shape[0] # number of centers
+
+        # Get the indices for all the points being sampled
+        indices = np.zeros((self.n_points,self.n_vertices),dtype=int)
+        for i in range(self.n_points):
+            c = (1-self.depths[i])*c1.T+self.depths[i]*c2.T
+            ijk,good = nt.coords_to_linvidxs(c,self.mask_img,mask=True)
+            indices[i] = ijk.T
+
+        # Compute mid-depth surface
+        mid_depth = (c1 + c2) / 2
+        midsurf = nno.Surface(mid_depth, f1)
+
+        for i in range(self.n_cent):
+            if np.mod(i,1000)==0:
+                print(f"Processing center {i} of {self.n_cent}")
+            dist_dict = midsurf.dijkstra_distance(self.center_indx[i],radius)
+            can_nodes = np.array([int(k) for k,v in dist_dict.items()])
+            can_dist = np.array([np.double(v) for k,v in dist_dict.items()])
+            can_voxels = indices[:,can_nodes].T.flatten()  # These are the voxels sorted by distance
+            _,corder = np.unique(can_voxels,return_index=True)  # Remove duplicates and return index of closest occurrance
+            a = np.array(['b','b','b','a','a','c','c'])
+            _, idx = np.unique(a, return_index=True)
+            print(a[np.sort(idx)])
+            if self.nvoxels is None:
+                vi=np.where([dist<(self.radius**2)])[0]
+            elif self.radius is None:
+                vi=np.argsort(dist)[:self.nvoxels]
+            else:
+                vi = np.where(dist<(self.radius**2))[0]
+                if len(vi)>self.nvoxels:
+                    vi=np.argsort(dist[vi])[:self.nvoxels]
+            self.voxlist.append(vi.astype('uint32'))
+            self.voxmin[i,:] = np.min(self.voxel_indx[:,vi],axis=1)
+            self.voxmax[i,:] = np.max(self.voxel_indx[:,vi],axis=1)
+            self.maxdist[i] = np.max(dist[vi])
 
 class SearchlightSet():
     def __init__(self,list_of_searchlights):
---
+++ test_surf.py
@@ -0,0 +1,37 @@
+import searchlight as sl
+import numpy as np
+import nibabel as nib
+
+def mvpa_mean_function(data):
+    # Example of MVPA function that returns a scalar as an output argument
+    return np.mean(data)
+
+def mvpa_multi_function(data):
+    # Example of MVPA function that returns a vector as an output argument
+    return np.mean(data,axis=1)
+
+def test_define_searchlight():
+    mySearchlight = sl.SearchlightSurface('left_hem')
+    surf = ['data/sub-02_space-32k_hemi-L_pial.surf.gii',
+            'data/sub-02_space-32k_hemi-L_white.surf.gii']
+    mask = 'data/sub-02_ses-s1_mask.nii'
+    mySearchlight.define(surf,mask,radius=20,nvoxels=100)
+    mySearchlight.save('data/searchlight.h5')
+
+def test_run_searchlight_mean():
+    datafiles = [f"data/sub-02_ses-s1_run-01_reg-{s:02d}_beta.nii" for s in range(5)]
+    S= sl.load('data/searchlight_new.h5')
+    results = S.run(datafiles, mvpa_mean_function, )
+    S.save_results(results,'data/output1.nii')
+
+def test_run_searchlight_multi():
+    datafiles = [f"data/sub-02_ses-s1_run-01_reg-{s:02d}_beta.nii" for s in range(5)]
+    S= sl.load('data/searchlight.h5')
+    results = S.run(datafiles, mvpa_multi_function, )
+    S.save_results(results,'data/output2.nii')
+
+
+if __name__ == '__main__':
+    test_define_searchlight()
+    test_run_searchlight_mean()
+    pass
